88e762792eb47a2ebe200541952a40c9cd5125cc
Prajata Samanta <prajatasamanta2204@gmail.com>
2021-08-09 17:47:23 +0530

    Initial Commit

---
+++ Swift Basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Basic
+//
+//  Created by Jarvis on 09/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Basic/Swift_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_BasicApp.swift
+//  Swift Basic
+//
+//  Created by Jarvis on 09/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
