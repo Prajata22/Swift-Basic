ca0dd7d2c3c95a31b2ea882ab6eedc12340457c0
Prajata Samanta <prajatasamanta2204@gmail.com>
2021-08-11 01:02:11 +0530

    Prajata

--- Swift Basic/App/ContentView.swift
+++ Swift Basic/App/ContentView.swift
@@ -2,38 +2,20 @@
 //  ContentView.swift
 //  Swift Basic
 //
-//  Created by Jarvis on 09/08/21.
+//  Created by Jarvis on 11/08/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+        
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Image("splash_image")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-                
-            
-            Spacer()
-            
-            Text("from")
-            
-            Text("Prajata Samanta")
-                .font(.title3)
-                .bold()
-                .frame(height: 30.0)
-        }
+        Text("")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            
-            
     }
 }
